3d5dd1edf20bf5e4066f5467efd15f3265d1bbb0
Adam Stallard <spanishgum@gmail.com>
2021-01-31 01:44:35 +0000

    play around with some src ideas

---
+++ src/details/testing/inputs.hpp
@@ -0,0 +1,6 @@
+#include "details/std_includes.hpp"
+
+template <typename T> vector<T> read_vector(string path) {
+    vector<T> ans;
+    return ans;
+}
--- src/questions/example/solution.hpp
+++ src/questions/example/solution.hpp
@@ -1,8 +1,16 @@
 #include "leet.hpp"
 
 class Solution {
+    using test_type = pair<int, tuple<vector<int>>>;
+
   public:
     int array_sum(const vector<int> &nums) {
         return accumulate(nums.cbegin(), nums.cend(), 0);
+    }
+
+    vector<test_type> tests() {
+        return {
+            make_pair(0, make_tuple(read_vector<int>("01-empty-array"))),
+        };
     }
 };
