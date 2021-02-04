9bcaede9df011f4283a54712d78e2f9ab2d5786c
Adam Stallard <spanishgum@gmail.com>
2021-02-04 06:23:30 +0000

    starting to work out some parsing strategies

--- src/details/inputs.hpp
+++ src/details/inputs.hpp
@@ -8,15 +8,75 @@
 
 namespace leet {
 
+struct FileStreamException : public std::exception {
+    std::string path;
+
+    template <typename PathType>
+    FileStreamException(const PathType &p) : path(p.string()) {}
+
+    const char *what() const throw() { return this->path.c_str(); }
+};
+
+struct BadInputException : public std::exception {
+    const char *what() const throw() { return "Bad input"; }
+};
+
 template <typename PathType>
 std::ifstream load_stream(PathType &&path) {
-    std::ifstream ifs{path.c_str()};
+    const std::string base_dir =
+        std::filesystem::path{"../src/questions/example/inputs"};
+    const std::filesystem::path p{std::forward<PathType>(path)};
+    auto full_path = base_dir / p;
+    std::ifstream ifs{full_path.c_str()};
+    if (!ifs) {
+        throw FileStreamException(full_path);
+    }
     return ifs;
+}
+
+template <typename T, typename Stream>
+std::vector<T> read_vector_impl(Stream &ifs) {
+    std::vector<T> ans;
+
+    // open the array
+    if (ifs.peek() == '[') {
+        ifs.get();
+    } else {
+        throw BadInputException();
+    }
+
+    // start parsing
+    T element;
+    // make sure stream is good
+    while (ifs) {
+        auto next = ifs.peek();
+        // check for end of input, or delimiter
+        // anything else is bad input
+        if (next == ']') {
+            break;
+        } else if (next == ',') {
+            ifs.get();
+        } else {
+            throw BadInputException();
+        }
+
+        // expect T to be readable
+        // anything else is bad input
+        if (ifs >> element) {
+            ans.push_back(element);
+        } else {
+            throw BadInputException();
+        }
+    }
+
+    return ans;
 }
 
 template <typename T, typename PathType>
 std::vector<T> read_vector(PathType &&path) {
     std::vector<T> ans;
+    auto ifs = load_stream(std::forward<PathType>(path));
+    ans = std::move(read_vector_impl<T>(ifs));
     return ans;
 }
 
--- src/questions/example/solution.hpp
+++ src/questions/example/solution.hpp
@@ -11,24 +11,27 @@ class Solution {
             leet::expect(0,
                          [&]() {
                              Solution s;
-                             auto v = leet::read_vector<int>("01-empty-array");
+                             auto v =
+                                 leet::read_vector<int>("01-empty-array.txt");
                              return s.array_sum(v);
                          }),
             leet::expect(0,
                          [&]() {
                              Solution s;
-                             auto v = leet::read_vector<int>("02-small-array");
+                             auto v =
+                                 leet::read_vector<int>("02-small-array.txt");
                              return s.array_sum(v);
                          }),
             leet::expect(1,
                          [&]() {
                              Solution s;
-                             auto v = leet::read_vector<int>("03-small-array");
+                             auto v =
+                                 leet::read_vector<int>("03-small-array.txt");
                              return s.array_sum(v);
                          }),
             leet::expect(0, [&]() {
                 Solution s;
-                auto v = leet::read_vector<int>("04-large-array");
+                auto v = leet::read_vector<int>("04-large-array.txt");
                 return s.array_sum(v);
             }));
     }
